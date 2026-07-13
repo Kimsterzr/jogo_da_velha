5ef5673187700ad4062e8cc39d05c3051f53c6b5
Kimsterzr <andrey.silveira53@gmail.com>
2026-07-13 15:57:20 -0300

    Highlight winner in verifica function

--- veia.js
+++ veia.js
@@ -31,14 +31,29 @@ function verifica(){
     let tabela = document.querySelector('table') 
     for (i = 0;i <= 2;i++){
         if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1]  == tabuleiro[i][2] && tabuleiro[i][0] != ""){
-            tabela.outerHTML = `<h1>${tabuleiro[i][0]} é o Vencedor</h1>`
+            if (tabuleiro[i][0] == 'X'){
+            tabela.outerHTML = `<h1><span style="color: red;">${tabuleiro[i][0]}</span> é o Vencedor</h1>`
+            }
+            else{
+                tabela.outerHTML = `<h1><span style="color: blue;">${tabuleiro[i][0]}</span> é o Vencedor</h1>`
+            }
         }
         if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i]  == tabuleiro[2][i] && tabuleiro[0][i] != ""){
-            tabela.outerHTML = `<h1>${tabuleiro[0][i]} é o Vencedor</h1>`
+           if (tabuleiro[0][i] == 'X'){
+            tabela.outerHTML = `<h1><span style="color: red;">${tabuleiro[0][i]}</span> é o Vencedor</h1>`
+            }
+            else{
+                tabela.outerHTML = `<h1><span style="color: blue;">${tabuleiro[0][i]}</span> é o Vencedor</h1>`
+            }
         }
     }
     if ((tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2] && tabuleiro[0][0] != "") || (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0] && tabuleiro[0][2] != "")){
-        tabela.outerHTML = `<h1>${tabuleiro[1][1]} é o Vencedor</h1>`
+        if (tabuleiro[1][1] == 'X'){
+            tabela.outerHTML = `<h1><span style="color: red;">${tabuleiro[1][1]}</span> é o Vencedor</h1>`
+        }
+        else{
+            tabela.outerHTML = `<h1><span style="color: blue;">${tabuleiro[1][1]}</span> é o Vencedor</h1>`
+        }
     }
     if (jogadas == 9){
         tabela.outerHTML = `<h1>Deu velha!</h1>`
